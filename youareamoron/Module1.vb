b975f956ac9c48a9a8c461e4a2f0a1146592a54e
IdotMaster1 <justapersom@gmail.com>
2022-04-13 23:57:44 +0200

    The first and probably last commit

---
+++ youareamoron/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hey, Jackass! Whats your name?")
+        Console.WriteLine("Name:")
+        Dim vadimblyat = Console.ReadLine()
+        Const V As String = ", you are a moron."
+        Console.WriteLine(vadimblyat + V)
+        Console.WriteLine("I made this program in less then 5 minutes lol")
+        Console.Write($"{Environment.NewLine}Press any key to exit...")
+        Console.ReadKey(True)
+    End Sub
+
+End Module
